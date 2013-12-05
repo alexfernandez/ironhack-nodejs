4113ed66df3d0ae5470ddb750f938d4728d05f1f
Alex Fernández <alex.fernandez@mediasmart.es>
2013-12-06 00:57:54 +0100

    Removed a couple of lines.

--- hello-world.js
+++ hello-world.js
@@ -2,8 +2,6 @@
 
 var net = require('net');
 
-var port = 1702;
-
 var server = net.createServer(function(connection) {
 	console.log('Connection open');
 	connection.write('Hello?\r\n');
@@ -12,7 +10,6 @@ var server = net.createServer(function(connection) {
 			connection.write('ERROR\r\n');
 		} else {
 			connection.write('world\r\n');
-			connection.end();
 			console.log('Connection closed');
 		}
 	});
